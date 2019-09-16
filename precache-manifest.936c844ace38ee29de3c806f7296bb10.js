765324f5e54b0afe26a837c42518dba06e27a841
Agmar Torres <agmartorres@gmail.com>
2019-09-16 07:06:34 -0300

    Updates

---
+++ precache-manifest.936c844ace38ee29de3c806f7296bb10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3837cc4cc12819af2bcc913b214330a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b329db0e4317a2f546f0",
+    "url": "/monsters-rolodex/static/css/main.adb21502.chunk.css"
+  },
+  {
+    "revision": "46012db1d69cedf8d1ab",
+    "url": "/monsters-rolodex/static/js/2.3d8af1b9.chunk.js"
+  },
+  {
+    "revision": "b329db0e4317a2f546f0",
+    "url": "/monsters-rolodex/static/js/main.13620c13.chunk.js"
+  },
+  {
+    "revision": "5f535753dcec63ef3476",
+    "url": "/monsters-rolodex/static/js/runtime~main.7352cc78.js"
+  }
+]);
